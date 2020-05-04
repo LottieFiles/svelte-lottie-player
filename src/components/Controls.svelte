edbcbb7b7c15296d6ac89fe82a0fdec37b554b27
Jawish Hameed <jawish@gmail.com>
2020-05-04 14:01:22 +0800

    Add color to progress and fix frame box styling

--- src/components/Controls.svelte
+++ src/components/Controls.svelte
@@ -1,78 +1,3 @@
-<script>
-  import { createEventDispatcher } from "svelte";
-
-  import ColorPicker from "./ColorPicker.svelte";
-  import Info from "./Info.svelte";
-  import Popover from "./Popover.svelte";
-  import { ControlsLayoutOptions, PlayerState } from "./utils";
-
-  // Define icon size
-  const ICON_SIZE = { width: 24, height: 24 };
-
-  // Create event dispatcher
-  const dispatch = createEventDispatcher();
-
-  export let animationData;
-  export let background;
-  export let currentState;
-  export let frame;
-  export let freeze;
-  export let instance;
-  export let layout = ControlsLayoutOptions;
-  export let loop;
-  export let play;
-  export let progress;
-  export let seek;
-  export let snapshot;
-  export let stop;
-  export let toggleZoom;
-  export let toggleLooping;
-  export let togglePlay;
-  export let totalFrames = 0;
-
-  let isZoomed = false;
-
-  $: isPlaying = currentState === PlayerState.Playing;
-  $: isPaused = currentState === PlayerState.Paused;
-  $: isStopped = currentState === PlayerState.Stopped;
-  $: formattedFrame = Math.round(frame);
-
-  const onHandleSeekChange = e => {
-    if (!instance || isNaN(e.target.value)) {
-      return;
-    }
-
-    const frame = (e.target.value / 100) * totalFrames;
-
-    seek(frame);
-  };
-
-  const onSelectBackground = e => {
-    dispatch("background", {
-      color: e.detail.hex
-    });
-  };
-
-  const onToggleZoom = () => {
-    isZoomed = !isZoomed;
-    toggleZoom();
-  };
-
-  const onNextFrame = () => {
-    const nextFrame = frame + 1;
-    if (nextFrame <= totalFrames) {
-      seek(nextFrame);
-    }
-  };
-
-  const oPreviousFrame = () => {
-    const prevFrame = frame - 1;
-    if (prevFrame >= 0) {
-      seek(prevFrame);
-    }
-  };
-</script>
-
 <style>
   .lottie-player-controls {
     align-items: center;
@@ -126,7 +51,7 @@
   }
   .progress::-webkit-slider-runnable-track {
     width: 100%;
-    height: 5px;
+    height: 3px;
     cursor: pointer;
     background: #ccc;
     border-radius: 3px;
@@ -135,17 +60,17 @@
     height: 15px;
     width: 15px;
     border-radius: 50%;
-    background: rgba(0, 107, 120, 0.8);
+    background: #0fccce;
     cursor: pointer;
     -webkit-appearance: none;
-    margin-top: -5px;
+    margin-top: -6px;
   }
   .progress:focus::-webkit-slider-controls-runnable-track {
     background: #999;
   }
   .progress::-moz-range-track {
     width: 100%;
-    height: 5px;
+    height: 3px;
     cursor: pointer;
     background: #ccc;
     border-radius: 3px;
@@ -154,12 +79,12 @@
     height: 15px;
     width: 15px;
     border-radius: 50%;
-    background: rgba(0, 107, 120, 0.8);
+    background: #0fccce;
     cursor: pointer;
   }
   .progress::-ms-track {
     width: 100%;
-    height: 5px;
+    height: 3px;
     cursor: pointer;
     background: transparent;
     border-color: transparent;
@@ -178,7 +103,7 @@
     height: 15px;
     width: 15px;
     border-radius: 50%;
-    background: rgba(0, 107, 120, 0.8);
+    background: #0fccce;
     cursor: pointer;
   }
   .progress:focus::-ms-fill-lower {
@@ -191,8 +116,8 @@
   .popover {
     padding: 10px;
     background: #fff;
-    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-      "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana,
+      sans-serif;
     font-size: 0.75rem;
     border-radius: 5px;
   }
@@ -209,7 +134,7 @@
     text-decoration: none;
   }
   .popover-snapshot a:before {
-    content: "⥼";
+    content: '⥼';
     margin-right: 5px;
   }
   .popover-snapshot .note {
@@ -229,6 +154,11 @@
     width: 40px;
     text-align: center;
     color: #999;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana,
+      sans-serif;
+    font-size: 0.75rem;
+    height: 22px;
+    padding: 0px;
   }
 
   .popover-background {
@@ -236,29 +166,99 @@
   }
 </style>
 
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  import ColorPicker from './ColorPicker.svelte';
+  import Info from './Info.svelte';
+  import Popover from './Popover.svelte';
+  import { ControlsLayoutOptions, PlayerState } from './utils';
+
+  // Define icon size
+  const ICON_SIZE = { width: 24, height: 24 };
+
+  // Create event dispatcher
+  const dispatch = createEventDispatcher();
+
+  export let animationData;
+  export let background;
+  export let currentState;
+  export let frame;
+  export let freeze;
+  export let instance;
+  export let layout = ControlsLayoutOptions;
+  export let loop;
+  export let play;
+  export let progress;
+  export let seek;
+  export let snapshot;
+  export let stop;
+  export let toggleZoom;
+  export let toggleLooping;
+  export let togglePlay;
+  export let totalFrames = 0;
+
+  let isZoomed = false;
+
+  $: isPlaying = currentState === PlayerState.Playing;
+  $: isPaused = currentState === PlayerState.Paused;
+  $: isStopped = currentState === PlayerState.Stopped;
+  $: formattedFrame = Math.round(frame);
+
+  const onHandleSeekChange = e => {
+    if (!instance || isNaN(e.target.value)) {
+      return;
+    }
+
+    const frame = (e.target.value / 100) * totalFrames;
+
+    seek(frame);
+  };
+
+  const onSelectBackground = e => {
+    dispatch('background', {
+      color: e.detail.hex,
+    });
+  };
+
+  const onToggleZoom = () => {
+    isZoomed = !isZoomed;
+    toggleZoom();
+  };
+
+  const onNextFrame = () => {
+    const nextFrame = frame + 1;
+    if (nextFrame <= totalFrames) {
+      seek(nextFrame);
+    }
+  };
+
+  const oPreviousFrame = () => {
+    const prevFrame = frame - 1;
+    if (prevFrame >= 0) {
+      seek(prevFrame);
+    }
+  };
+</script>
+
 <div class="lottie-player-controls">
   {#each layout as item}
     {#if item === 'playpause'}
-      <button
-        class="control btn"
-        on:click={togglePlay}
-        class:active={isPlaying || isPaused}>
+      <button class="control btn" on:click="{togglePlay}" class:active="{isPlaying || isPaused}">
         {#if isPlaying}
           <svg {...ICON_SIZE}>
-            <path
-              d="M14.016 5.016H18v13.969h-3.984V5.016zM6
-              18.984V5.015h3.984v13.969H6z" />
+            <path d="M14.016 5.016H18v13.969h-3.984V5.016zM6 18.984V5.015h3.984v13.969H6z"></path>
           </svg>
         {:else}
           <svg {...ICON_SIZE}>
-            <path d="M8.016 5.016L18.985 12 8.016 18.984V5.015z" />
+            <path d="M8.016 5.016L18.985 12 8.016 18.984V5.015z"></path>
           </svg>
         {/if}
       </button>
     {:else if item === 'stop'}
-      <button class="control btn" on:click={stop} class:active={isStopped}>
+      <button class="control btn" on:click="{stop}" class:active="{isStopped}">
         <svg {...ICON_SIZE}>
-          <path d="M6 6h12v12H6V6z" />
+          <path d="M6 6h12v12H6V6z"></path>
         </svg>
       </button>
     {:else if item === 'progress'}
@@ -268,17 +268,18 @@
         min="0"
         step="1"
         max="100"
-        bind:value={progress}
-        on:input={onHandleSeekChange}
-        on:mousedown={freeze}
-        on:mouseup={play} />
+        bind:value="{progress}"
+        on:input="{onHandleSeekChange}"
+        on:mousedown="{freeze}"
+        on:mouseup="{play}"
+      />
     {:else if item === 'loop'}
-      <button class="control btn" on:click={toggleLooping} class:active={loop}>
+      <button class="control btn" on:click="{toggleLooping}" class:active="{loop}">
         <svg {...ICON_SIZE}>
           <path
-            d="M17.016 17.016v-4.031h1.969v6h-12v3l-3.984-3.984
-            3.984-3.984v3h10.031zM6.984 6.984v4.031H5.015v-6h12v-3l3.984
-            3.984-3.984 3.984v-3H6.984z" />
+            d="M17.016 17.016v-4.031h1.969v6h-12v3l-3.984-3.984 3.984-3.984v3h10.031zM6.984
+            6.984v4.031H5.015v-6h12v-3l3.984 3.984-3.984 3.984v-3H6.984z"
+          ></path>
         </svg>
       </button>
     {:else if item === 'background'}
@@ -287,56 +288,45 @@
           <button class="btn" slot="target">
             <svg {...ICON_SIZE}>
               <path
-                d="M12 3.1L7.05 8.05a7 7 0 1 0 9.9 0L12 3.1zm0-2.828l6.364
-                6.364a9 9 0 1 1-12.728 0L12 .272zM7 13h10a5 5 0 0 1-10 0z" />
+                d="M12 3.1L7.05 8.05a7 7 0 1 0 9.9 0L12 3.1zm0-2.828l6.364 6.364a9 9 0 1 1-12.728 0L12 .272zM7 13h10a5 5
+                0 0 1-10 0z"
+              ></path>
             </svg>
           </button>
           <div slot="content" class="popover popover-background">
-            <ColorPicker color={background} on:color={onSelectBackground} />
+            <ColorPicker color="{background}" on:color="{onSelectBackground}" />
           </div>
         </Popover>
       </div>
     {:else if item === 'snapshot'}
-      <div class="control" on:mouseout={play} on:mouseover={freeze}>
-        <Popover
-          color="#fff"
-          on:mousewheel={e => seek(frame + (e.deltaY > 0 ? -1 : 1))}>
+      <div class="control" on:mouseout="{play}" on:mouseover="{freeze}">
+        <Popover color="#fff" on:mousewheel="{e => seek(frame + (e.deltaY > 0 ? -1 : 1))}">
           <button class="btn" slot="target">
             <svg {...ICON_SIZE}>
               <path
-                d="M11.993 14.407l-1.552 1.552a4 4 0 1
-                1-1.418-1.41l1.555-1.556-4.185-4.185 1.415-1.415 4.185 4.185
-                4.189-4.189 1.414 1.414-4.19 4.19 1.562 1.56a4 4 0 1 1-1.414
-                1.414l-1.561-1.56zM7 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm10 0a2 2 0
-                1 0 0-4 2 2 0 0 0 0 4zm2-7V5H5v8H3V4a1 1 0 0 1 1-1h16a1 1 0 0 1
-                1 1v9h-2z" />
+                d="M11.993 14.407l-1.552 1.552a4 4 0 1 1-1.418-1.41l1.555-1.556-4.185-4.185 1.415-1.415 4.185 4.185
+                4.189-4.189 1.414 1.414-4.19 4.19 1.562 1.56a4 4 0 1 1-1.414 1.414l-1.561-1.56zM7 20a2 2 0 1 0 0-4 2 2 0
+                0 0 0 4zm10 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm2-7V5H5v8H3V4a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v9h-2z"
+              ></path>
             </svg>
           </button>
           <div slot="content" class="popover popover-snapshot">
             <h5>Frame {formattedFrame}</h5>
-            <a href="#downloadsvg" on:click={() => snapshot(true)}>
-              Download SVG
-            </a>
-            <a href="#downloadsvg" on:click={() => snapshot(true)}>
-              Download PNG
-            </a>
+            <a href="#downloadsvg" on:click="{() => snapshot(true)}">Download SVG</a>
+            <a href="#downloadsvg" on:click="{() => snapshot(true)}">Download PNG</a>
             <i class="note">Scroll with mousewheel to find exact frame</i>
           </div>
         </Popover>
       </div>
     {:else if item === 'zoom'}
-      <button class="control btn" on:click={onToggleZoom}>
+      <button class="control btn" on:click="{onToggleZoom}">
         {#if isZoomed}
           <svg {...ICON_SIZE}>
-            <path
-              d="M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8
-              15v6H6v-4H2v-2h6z" />
+            <path d="M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8 15v6H6v-4H2v-2h6z"></path>
           </svg>
         {:else}
           <svg {...ICON_SIZE}>
-            <path
-              d="M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16
-              16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z" />
+            <path d="M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z"></path>
           </svg>
         {/if}
       </button>
@@ -346,9 +336,9 @@
           <button class="btn" slot="target">
             <svg {...ICON_SIZE}>
               <path
-                d="M15 4H5v16h14V8h-4V4zM3 2.992C3 2.444 3.447 2 3.999 2H16l5
-                5v13.993A1 1 0 0 1 20.007 22H3.993A1 1 0 0 1 3 21.008V2.992zM11
-                11h2v6h-2v-6zm0-4h2v2h-2V7z" />
+                d="M15 4H5v16h14V8h-4V4zM3 2.992C3 2.444 3.447 2 3.999 2H16l5 5v13.993A1 1 0 0 1 20.007 22H3.993A1 1 0 0
+                1 3 21.008V2.992zM11 11h2v6h-2v-6zm0-4h2v2h-2V7z"
+              ></path>
             </svg>
           </button>
           <div slot="content" class="popover popover-info">
@@ -361,32 +351,33 @@
         <input
           class="frame-number"
           type="text"
-          bind:value={formattedFrame}
-          on:mouseout={play}
-          on:mouseover={freeze}
-          on:input={e => seek(e.target.value)}
-          on:mousewheel={e => seek(frame + (e.deltaY > 0 ? -1 : 1))} />
+          bind:value="{formattedFrame}"
+          on:mouseout="{play}"
+          on:mouseover="{freeze}"
+          on:input="{e => seek(e.target.value)}"
+          on:mousewheel="{e => seek(frame + (e.deltaY > 0 ? -1 : 1))}"
+        />
       </div>
     {:else if item === 'nextFrame'}
-      <button class="control btn" on:click={onNextFrame}>
+      <button class="control btn" on:click="{onNextFrame}">
         <svg {...ICON_SIZE}>
           <path
-            d="M12 13.333l-9.223 6.149A.5.5 0 0 1 2 19.066V4.934a.5.5 0 0 1
-            .777-.416L12 10.667V4.934a.5.5 0 0 1 .777-.416l10.599 7.066a.5.5 0 0
-            1 0 .832l-10.599 7.066a.5.5 0 0 1-.777-.416v-5.733z" />
+            d="M12 13.333l-9.223 6.149A.5.5 0 0 1 2 19.066V4.934a.5.5 0 0 1 .777-.416L12 10.667V4.934a.5.5 0 0 1
+            .777-.416l10.599 7.066a.5.5 0 0 1 0 .832l-10.599 7.066a.5.5 0 0 1-.777-.416v-5.733z"
+          ></path>
         </svg>
       </button>
     {:else if item === 'previousFrame'}
-      <button class="control btn" on:click={oPreviousFrame}>
+      <button class="control btn" on:click="{oPreviousFrame}">
         <svg {...ICON_SIZE}>
           <path
-            d="M12 10.667l9.223-6.149a.5.5 0 0 1 .777.416v14.132a.5.5 0 0
-            1-.777.416L12 13.333v5.733a.5.5 0 0 1-.777.416L.624 12.416a.5.5 0 0
-            1 0-.832l10.599-7.066a.5.5 0 0 1 .777.416v5.733z" />
+            d="M12 10.667l9.223-6.149a.5.5 0 0 1 .777.416v14.132a.5.5 0 0 1-.777.416L12 13.333v5.733a.5.5 0 0
+            1-.777.416L.624 12.416a.5.5 0 0 1 0-.832l10.599-7.066a.5.5 0 0 1 .777.416v5.733z"
+          ></path>
         </svg>
       </button>
     {:else if item === 'spacer'}
-      <span class="control spacer" />
+      <span class="control spacer"></span>
     {/if}
   {/each}
 </div>
